a4a60e3671d4f01c63653b2518c8e813d8d7faf1
Samuel Ramos <104317445+Thesamramos@users.noreply.github.com>
2024-02-21 10:31:21 -0400

    CRUD - Read finalizado

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 tasks = []
 task_id_control = 1
 
-@app.route("/tasks", methods=["POST"])
+@app.route('/tasks', methods=['POST'])
 def create_task():
     global task_id_control
     data = request.get_json()
@@ -15,6 +15,24 @@ def create_task():
     tasks.append(new_task)
     print(tasks)
     return jsonify({"mensagem": "Nova tarefa criada com sucesso"})
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    tasks_list = [task.to_dict() for task in tasks]
+    output = {
+        "tasks": tasks_list,
+        "total_tasks": len(tasks_list)
+    }
+    return jsonify(output)
+
+@app.tpute('/tasks/<int:id>', method=['GET'])
+def get_task(id):
+    task = none
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+    
+    return jsonify({"mensagem": "Não foi possível encontrar a atividade"}), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
