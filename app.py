ae9096b0d8bf9fd7333f9f7ca6aecc6a663d8273
Samuel Ramos <104317445+Thesamramos@users.noreply.github.com>
2024-02-21 16:50:39 -0400

    CRUD - Delete

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ def uptade_task(id):
     for t in tasks:
         if t.id == id:
             task = t
+            break
     if task == None:
         return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
 
@@ -47,6 +48,19 @@ def uptade_task(id):
     task.description = data["description"]
     task.completed = data["completed"]
     return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+
+    if not task:
+        return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+    tasks.remove(task)
+    return jsonify({"message": "Tarefa deletada com sucesso"})
 
 if __name__ == '__main__':
     app.run(debug=True)
